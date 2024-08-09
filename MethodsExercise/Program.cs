a11c965f280781777b67aba0adf0adad6a34abc9
Isaac Weiland <iweiland7777@gmail.com>
2024-08-09 13:00:35 -0400

    Wrote story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+        }
+
+        static void MadLib()
+        {
+            Console.WriteLine("Enter name. \n");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nPlease enter an animal.\n");
+            string animal = Console.ReadLine();
+            Console.WriteLine("\nPlease enter your favorite beverage.\n");
+            string drink = Console.ReadLine();
+            Console.WriteLine("\nPlease enter a day of the week.\n");
+            string day = Console.ReadLine();
+            Console.WriteLine("\nPlease enter a holiday.\n");
+            string celebration = Console.ReadLine();
+            Console.WriteLine($"{name} was a {animal} who loved {celebration}.\nWhen it came around this year on {day} " +
+                              $"it got out its favorite drink, {drink}.");
         }
     }
 }
