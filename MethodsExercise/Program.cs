91158b5ae2942135c0d264bba44ff69c94ff73fe
Isaac Weiland <iweiland7777@gmail.com>
2024-08-09 16:27:45 -0400

    Added  the second part of the exercise. Made the first question more polite.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,12 +4,17 @@
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine(Add(3, 4));
+            Console.WriteLine(Mult(6, 20, 2));
+            Console.WriteLine(Sub(10, 4));
+            Console.WriteLine(Div(64, 8));
             MadLib();
         }
 
         static void MadLib()
         {
-            Console.WriteLine("Enter name. \n");
+            Console.WriteLine("Please enter a name. \n");
             string name = Console.ReadLine();
             Console.WriteLine("\nPlease enter an animal.\n");
             string animal = Console.ReadLine();
@@ -21,6 +26,36 @@
             string celebration = Console.ReadLine();
             Console.WriteLine($"{name} was a {animal} who loved {celebration}.\nWhen it came around this year on {day} " +
                               $"it got out its favorite drink, {drink}.");
+        }
+
+        static int Add(params int[] a)
+        {
+            int sum = 0;
+            foreach (int value in a)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+
+        static int Mult(params int[] a)
+        {
+            int sum = 1;
+            foreach (int value in a)
+            {
+                sum *= value;
+            }
+            return sum;
+        }
+
+        static int Sub(int a, int b)
+        {
+            return  a - b;
+        }
+
+        static int Div(int a, int b )
+        {
+            return a / b;
         }
     }
 }
